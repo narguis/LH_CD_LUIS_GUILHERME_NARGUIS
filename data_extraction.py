5ea5dfcb258a32c77a4500fd37cd9df6535746c3
Guilherme Narguis <narguis.guilherme@gmail.com>
2024-07-10 15:10:39 -0300

    analisando correlação

--- data_extraction.py
+++ data_extraction.py
@@ -4,16 +4,33 @@ import seaborn as sns
 
 df = pd.read_csv("desafio_indicium_imdb.csv")
 
+print(df.describe())
+print(df.info())
+
+
+df['Gross'].fillna(df['Gross'].mean(), inplace=True)
+
 # There are some empty cells in Gross, Meta Score and Certificate
-# print(df.isnull().sum())
-# df['Meta_score'].fillna(df['Meta_score'].mean(), inplace=True)
-# df.dropna(inplace=True)
+print(df.isnull().sum())
+df['Meta_score'].fillna(df['Meta_score'].mean(), inplace=True)
+df.dropna(inplace=True)
 
 # Analyzing IMDB distribution
-plt.figure(figsize=(10, 6))
-sns.histplot(df['IMDB_Rating'], bins=20, kde=True)
-plt.title('IMDB Rating Distribution')
-plt.show()
+# plt.figure(figsize=(10, 6))
+# sns.histplot(df['IMDB_Rating'], bins=20, kde=True)
+# plt.title('IMDB Rating Distribution')
+# plt.show()
 
+
+# Correlation matrix
+correlation_matrix = df.corr()
+
+plt.figure(figsize=(10,6))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+plt.title('Correlation Matrix')
+# plt.show()
+
+
+print(correlation_matrix)
 
 print('texto pro debug')
